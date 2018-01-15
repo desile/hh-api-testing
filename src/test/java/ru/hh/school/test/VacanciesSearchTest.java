198dd498f6920e21beb10162f63507b148fff7d1
pv.larkin <pv.larkin@lpr.jet.msk.su>
2018-01-15 23:58:29 +0300

    Add more tests

--- src/test/java/ru/hh/school/test/VacanciesSearchTest.java
+++ src/test/java/ru/hh/school/test/VacanciesSearchTest.java
@@ -3,14 +3,14 @@ package ru.hh.school.test;
 import static io.restassured.RestAssured.given;
 import static org.hamcrest.Matchers.hasKey;
 import static org.hamcrest.Matchers.is;
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.*;
 
 import io.restassured.RestAssured;
 import io.restassured.path.json.JsonPath;
 import org.junit.BeforeClass;
 import org.junit.Test;
 
+import java.util.Collections;
 import java.util.Map;
 
 public class VacanciesSearchTest {
@@ -77,11 +77,68 @@ public class VacanciesSearchTest {
     @Test
     public void testSearchTextInVacancyNames(){
         JsonPath jsonPathResponse = given()
-                .queryParam("text", search_word)
-                .queryParam("search_field", "name")
+                .queryParam("text", "NAME:" + search_word)
                 .get("/vacancies").jsonPath();
         assertTrue(jsonPathResponse.getList("items.name", String.class)
                 .stream().allMatch(s -> s.toLowerCase().contains(search_word)));
     }
+
+    @Test
+    public void testSearchTextIsIgnoringCase() {
+        JsonPath upperCaseResponse = given()
+                .queryParam("text", "NAME:" + search_word.toUpperCase())
+                .get("/vacancies").jsonPath();
+        JsonPath lowerCaseResponse = given()
+                .queryParam("text", "NAME:" + search_word.toLowerCase())
+                .get("/vacancies").jsonPath();
+        assertEquals(upperCaseResponse.getInt("items.size()"), lowerCaseResponse.getInt("items.size()"));
+        for(int i = 0; i < upperCaseResponse.getInt("items.size()"); i++){
+            assertEquals(upperCaseResponse.getList("items.id").get(i), lowerCaseResponse.getList("items.id").get(i));
+        }
+    }
+
+    @Test
+    public void testEmptyTextInSearchActingLikeSearchWithNoTextParameter() {
+        JsonPath responseWithEmptyText = given()
+                .queryParam("text", "")
+                .get("/vacancies").jsonPath();
+        JsonPath responseWithoutText = given()
+                .get("/vacancies").jsonPath();
+        assertEquals(responseWithEmptyText.getInt("found"), responseWithoutText.getInt("found"));
+    }
+
+    @Test
+    public void testTextInSearchIsNotPartOfWord() {
+        JsonPath wholeWordResponse = given()
+                .queryParam("text", "NAME:" + search_word)
+                .get("/vacancies").jsonPath();
+        JsonPath partWordResponse = given()
+                .queryParam("text", "NAME:" + search_word.substring(0,4))
+                .queryParam("search_field", "name")
+                .get("/vacancies").jsonPath();
+        assertFalse(wholeWordResponse.getInt("found") <= partWordResponse.getInt("found") );
+    }
+
+    @Test
+    public void testTextInSearchWithAsteriskIsPartOfWord() {
+        JsonPath wholeWordResponse = given()
+                .queryParam("text", "NAME:" + search_word)
+                .get("/vacancies").jsonPath();
+        JsonPath partWordResponse = given()
+                .queryParam("text", "NAME:" + search_word.substring(0,4) + "*")
+                .get("/vacancies").jsonPath();
+        assertTrue(wholeWordResponse.getInt("found") <= partWordResponse.getInt("found") );
+    }
+
+    @Test
+    public void testVeryLongTextInSearchShouldActLikeNoTextInSearch() {
+        JsonPath veryLongTextResponse = given()
+                .queryParam("text", String.join("", Collections.nCopies(100, "asdf")))
+                .get("/vacancies").jsonPath();
+        JsonPath responseWithoutText = given()
+                .get("/vacancies").jsonPath();
+        assertEquals(veryLongTextResponse.getInt("found"), responseWithoutText.getInt("found"));
+    }
+
 
 }
